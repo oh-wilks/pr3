69b8a82189a218670a83b879ef48dbc0bd6d61d3
NormanWilcox <wilcoxivan@hotmal.com>
2023-04-11 22:17:36 -0700

    clases

--- javascript/btc_CurrentValue.js
+++ javascript/btc_CurrentValue.js
@@ -1,64 +1,83 @@
-// Definir el endpoint de API y sus parametros de consulta
-const apiEndpoint = "https://api.binance.com/api/v3/ticker/price";
-const symbol = "BTCUSDT";
-
-// Construccion de la URL de la API
-const apiUrl = `${apiEndpoint}?symbol=${symbol}`;
-
-// Hacer Fetch valor de BTC de la API
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    // Actualizar el HTML con el valor actual
-    const btcValueUsd = parseFloat(data.price).toFixed(2);
-    document.getElementById("btc-value").textContent = `$${btcValueUsd}`;
-  })
-  .catch((error) => {
-    console.error(error);
-    document.getElementById("btc-value").textContent =
-      "Error loading BTC value";
-  });
-
-// Obtener precios del BCT de la API de Binance 
-const api_url =
-  "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=365";
-async function getBitcoinPrices() {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  const prices = data.map((d) => parseFloat(d[4])); // Extraer precios de la data
-  return prices;
-}
-
-// Crear grafico de BTCUSDT
-async function createChart() {
-  const prices = await getBitcoinPrices();
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: Array.from(Array(prices.length).keys()), // Usar array como etiquetas 
-      datasets: [
-        {
-          label: "BTCUSDT",
-          data: prices,
-          fill: false,
-          borderColor: "rgb(75, 192, 192)",
-          tension: 0.1,
+class BinanceAPI {
+    constructor() {
+      this.apiEndpoint = "https://api.binance.com/api/v3";
+    }
+  
+    async getTickerPrice(symbol) {
+      const apiUrl = `${this.apiEndpoint}/ticker/price?symbol=${symbol}`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      if (data && data.price) {
+        return parseFloat(data.price).toFixed(2);
+      } else {
+        throw new Error("Failed to get ticker price");
+      }
+    }
+  
+    async getKlines(symbol, interval, limit) {
+      const apiUrl = `${this.apiEndpoint}/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      const prices = data.map((d) => parseFloat(d[4]));
+      return prices;
+    }
+  }
+  
+  class ChartManager {
+    constructor(canvasId, tickerId) {
+      this.canvas = document.getElementById(canvasId);
+      this.ctx = this.canvas.getContext("2d");
+      this.chart = null;
+      this.tickerElement = document.getElementById(tickerId);
+      this.api = new BinanceAPI();
+    }
+  
+    async updateTicker(symbol) {
+      try {
+        const price = await this.api.getTickerPrice(symbol);
+        this.tickerElement.textContent = `$${price}`;
+      } catch (error) {
+        console.error(error);
+        this.tickerElement.textContent = "Error loading ticker value";
+      }
+    }
+  
+    async createChart(symbol, interval, limit) {
+      const prices = await this.api.getKlines(symbol, interval, limit);
+      if (this.chart) {
+        this.chart.destroy(); // eliminar grafico existente
+      }
+      this.chart = new Chart(this.ctx, {
+        type: "line",
+        data: {
+          labels: Array.from(Array(prices.length).keys()),
+          datasets: [
+            {
+              label: symbol,
+              data: prices,
+              fill: false,
+              borderColor: "#F0000",
+              tension: 0.1,
+            },
+          ],
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          ticks: {
-            callback: function (value, index, values) {
-              return "$" + value.toLocaleString(); // Agregar signo de $
+        options: {
+          scales: {
+            y: {
+              ticks: {
+                callback: function (value, index, values) {
+                  return "$" + value.toLocaleString();
+                },
+              },
             },
           },
         },
-      },
-    },
-  });
-}
-
-createChart();
+      });
+      this.updateTicker(symbol);
+    }
+  }
+  
+  // Ejemplo con BTC
+  const chartManager = new ChartManager("myChart", "btc-value");
+  chartManager.createChart("BTCUSDT", "1d", 30);
+  

af8026a600d444bf70859834c795876d39d2b3ec
Saul P <saulpulido52@gmail.com>
2023-04-23 21:25:43 -0700

    se modifica el estilo de las tarjetas y el footer

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,13 @@
 const temaClaro = () => {
   document.documentElement.setAttribute("data-bs-theme", "light");
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
-  document
-    .querySelector("#nav")
-    .setAttribute("class", "navbar navbar-dark bg-dark shadow-lg ");
+  document.querySelector("#nav").setAttribute("class", "navbar navbar-dark bg-dark shadow-lg ");
+  document.querySelector("footer").setAttribute("class", "bg-light");
+    
 };
 const temaOscuro = () => {
   document.documentElement.setAttribute("data-bs-theme", "dark");
-
+  document.querySelector("footer").setAttribute("class", "bg-dark");
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
   document
     .querySelector("#nav")

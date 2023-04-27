87cd9707540b3e8a3e939bb6f646ceea8c531eb0
Saul P <saulpulido52@gmail.com>
2023-04-27 12:16:04 -0700

    se sigue revisando los cambios de grafica

--- javascript/btc_CurrentValue.js
+++ javascript/btc_CurrentValue.js
@@ -1,28 +1,17 @@
 const API_ENDPOINT = "https://api.binance.com/api/v3";
 const ERROR_TICKER_PRICE = "Failed to get ticker price";
 const ERROR_KLINES = "Failed to get klines";
-// Seleccionar los elementos del DOM
-const diaBtn = document.querySelector('.dia');
-const semanaBtn = document.querySelector('.semana');
-const mesBtn = document.querySelector('.mes');
-const anioBtn = document.querySelector('.anio');
-const canvas = document.getElementById('myChart');
-const dateInterval = document.getElementById("dateInterval")
-let myChart = null
 
-function clearActiveButtons() {
-  diaBtn.classList.remove('active');
-  semanaBtn.classList.remove('active');
-  mesBtn.classList.remove('active');
-  anioBtn.classList.remove('active');
-}
+
+const dateInterval = document.getElementById("dateInterval");
+let chart = null;
+
+
 class BinanceAPI {
-  constructor() {
-    this.apiEndpoint = API_ENDPOINT;
-  }
+  
 
   async getTickerPrice(symbol) {
-    const apiUrl = `${this.apiEndpoint}/ticker/price?symbol=${symbol}`;
+    const apiUrl = `${API_ENDPOINT}/ticker/price?symbol=${symbol}`;
     const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error(ERROR_TICKER_PRICE);
@@ -35,7 +24,8 @@ class BinanceAPI {
   }
 
   async getKlines(symbol, interval, limit) {
-    const apiUrl = `${this.apiEndpoint}/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
+    const apiUrl = `${API_ENDPOINT}/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`;
+
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
@@ -94,12 +84,12 @@ class ChartManager {
     if (!this.tickerElement) {
       throw new Error(`Invalid ticker ID: ${tickerId}`);
     }
-    this.canvas = document.getElementById(canvasId);
+    this.canvas = this.canvas = document.getElementById("myChart");
     if (!this.canvas) {
       throw new Error(`Invalid canvas ID: ${canvasId}`);
     }
     this.ctx = this.canvas.getContext("2d");
-    this.chart = null;
+    
     this.api = new BinanceAPI();
   }
 
@@ -124,10 +114,10 @@ class ChartManager {
       return date.toISOString().slice(0, 10);
     });
     
-    if (this.chart) {
-      this.chart.destroy();
+    if (chart) {
+      chart.destroy();
     }
-    this.chart = new Chart(this.ctx, {
+    chart = new Chart(this.ctx, {
       type: "line",
       data: {
         labels: date,
@@ -163,6 +153,24 @@ class ChartManager {
     document.querySelector(".spinner").remove();
   }
 }
+
+
+
+dateInterval.addEventListener("change", function(event) {
+  
+  const tickerSelect = document.getElementById("ticker-select");
+  selectedValue = tickerSelect.value;
+
+  interval = event.target.value;
+  chart.destroy();
+  console.log(interval)
+  console.log(selectedValue)
+  const chartManager = new ChartManager(selectedValue, interval, 30)
+  
+  chartManager.createChart(selectedValue, interval, 30);
+  
+  
+})
 
 
 // dropdown
@@ -201,16 +209,10 @@ async function populateTickerSelect(callback) {
 window.addEventListener("load", async () => {
   const chartManager = new ChartManager("myChart", "btc-value");
   populateTickerSelect(selectedValue => {
-    chartManager.createChart(selectedValue, "1d", 15);
+    chartManager.createChart(selectedValue, "1d", 30);
   });
 });
 
-dateInterval.addEventListener("change", function(event){
-  interval = event.target.value
-  chart.destroy()
-  getKlines()
-
-})
 
 const ticker_btcusdt = new TickerManager("btc-value");
 ticker_btcusdt.updateTicker("BTCUSDT");

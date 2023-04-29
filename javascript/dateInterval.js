84c3379c21a6992e7f22dfa7db14aeca0cecaf8d
NormanWilcox <wilcoxivan@hotmal.com>
2023-04-29 00:11:19 -0700

    interval-btns

---
+++ javascript/dateInterval.js
@@ -0,0 +1,17 @@
+export const intervals = {
+  interval1: {
+    name: "1 anio",
+    interval: "1w",
+    limit: 52,
+  },
+  interval2: {
+    name: "7 dias",
+    interval: "1d",
+    limit: 7,
+  },
+  interval3: {
+    name: "1 mes",
+    interval: "1d",
+    limit: 30,
+  },
+};

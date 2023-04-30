672ef9e0cbfc291fdfc57ff77df092270391d1b2
NormanWilcox <wilcoxivan@hotmal.com>
2023-04-29 21:12:45 -0700

    conflicto dropdown-grafico

--- javascript/tickerDropDown.js
+++ javascript/tickerDropDown.js
@@ -1,31 +1,41 @@
 import { API_ENDPOINT } from "./btc_CurrentValue.js";
 
-// dropdown
 export async function populateTickerSelect(callback) {
+  try {
     const apiUrl = `${API_ENDPOINT}/exchangeInfo`;
     const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    
     const { symbols } = await response.json();
-  
     const tickerSelect = document.getElementById("ticker-select");
-  
-    // ordenar alfabeticamente
     symbols.sort((a, b) => a.symbol.localeCompare(b.symbol));
-  
-    // agregar tickers al dropdown
-    symbols.forEach(symbol => {
+
+    const symbolObjects = symbols.map((symbol) => ({
+      baseAsset: symbol.baseAsset,
+      quoteAsset: symbol.quoteAsset,
+    }));
+    // iterando el objecto symbol para mostrar los tickers
+    symbols.forEach((symbol) => {
       const option = document.createElement("option");
       option.value = symbol.symbol;
-      option.textContent = symbol.symbol;
+      option.textContent = `${symbol.baseAsset} - ${symbol.quoteAsset}`;
       tickerSelect.appendChild(option);
     });
-  
+
+    // valor default primer carga, despues se guarda en local storage
+    let defaultValue = localStorage.getItem("ticker-select-default-value") || symbols[0].symbol;
+    callback(defaultValue);
+
     tickerSelect.addEventListener("change", () => {
       const selectedValue = tickerSelect.value;
+      localStorage.setItem("ticker-select-default-value", selectedValue);
       callback(selectedValue);
     });
-    // valor default
-    const defaultValue = "BTCUSDT";
-    callback(defaultValue);
+  } catch (error) {
+    console.error("Error", error);
   }
+}
 
-  
